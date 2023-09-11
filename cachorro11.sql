fbda62b56a659612eccf32e6f2408e77ffc36d55
Aumigo <141048312+Aumigo@users.noreply.github.com>
2023-09-11 12:25:11 -0300

    Add files via upload

---
+++ cachorro11.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE adocao_animais;
+USE adocao_animais;
+
+create table cachorro (
+id int key, 
+nome varchar(50),
+ sexo varchar (5),
+ idade varchar(50),
+ porte varchar (10),
+ descricao varchar(50),
+ nome_arq varchar(100), 
+ ext_arq varchar(5));
+ 
+
+
+
